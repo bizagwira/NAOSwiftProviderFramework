41f2b8d24b9f089dd1e3e499f09b9d94a2cf90d5
Honoré Bizagwira <honore.bizagwira@polestar.eu>
2020-01-21 13:01:50 +0100

    Create a NAOSDK wrapper for the Swift users

---
+++ Example/Pods/Target Support Files/NAOSwiftProviderFramework/NAOSwiftProviderFramework-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_NAOSwiftProviderFramework : NSObject
+@end
+@implementation PodsDummy_NAOSwiftProviderFramework
+@end
---
+++ Example/Pods/Target Support Files/Pods-NAOSwiftProviderFramework_Example/Pods-NAOSwiftProviderFramework_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NAOSwiftProviderFramework_Example : NSObject
+@end
+@implementation PodsDummy_Pods_NAOSwiftProviderFramework_Example
+@end
